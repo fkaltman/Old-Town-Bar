dff1fcac47b593af9063a3f87cd05115e0f32ccc
Carol Park <carol@Carols-MacBook.local>
2019-07-29 16:27:10 -0400

    about and reviews

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import About from './components/About';
+import Reviews from './components/Reviews';
 
 function App() {
   return (
     <div className="App">
-      
+      <About />
+      <Reviews />
     </div>
   );
 }
