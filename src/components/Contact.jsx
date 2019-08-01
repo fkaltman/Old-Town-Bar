41ad3f88c744dbf28bf3469954a4514dfa03d5eb
Francine Altman <francinekline@gmail.com>
2019-08-01 17:35:23 -0400

    adjust to new contact css

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,21 +1,17 @@
 import React from 'react';
-import bigcontact from "../assets/bigcontact.png"
 import googlemap from "../assets/googlemap.png"
 import address from "../assets/address.png"
-import subway4 from "../assets/subway4.png"
-import subway8 from "../assets/subway8.png"
 
-export default function Contact () {
+
+export default function Contact() {
 
   return (
-    <div className="contact-page" id="contact-us">
-      <a href="contact-us"></a>
-      <img className="big-contact" src={bigcontact} alt="Contact"/>
-      <img className="map" src={googlemap} alt="Google Map" />
-      <img className="address" src={address} alt="Address" />
-      <img className="subway4" src={subway4} alt="Subway symbols" />
-      <img className="subway8" src={subway8} alt="Subway symbols" />
-      
+    <div className="contact-page">
+      <h2 className="contact">Contact</h2>
+      <div className="map-and-address">
+        <img className="map" src={googlemap} alt="Google Map" />
+        <img className="address" src={address} alt="Address" />
+      </div>
     </div>
   )
 }
