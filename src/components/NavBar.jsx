0705849f10482c2a4dd72d245647935c54c56412
bingi <luis.carbonel.gk@hotmail.com>
2019-08-04 19:40:38 -0400

    nav style

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,24 +1,40 @@
 import React from 'react'
 
-export default class Navigation extends React.Component{
+export default class Navigation extends React.Component {
   constructor() {
     super()
   }
-  
+
 
   render() {
     return (
-      <div className="nav-container">
+      <div className="nav-container header">
+        {/* 
+        <input className="menu-btn" type="checkbox" id="menu-btn" />
+        <label className="menu-icon" for="menu-btn"><span className="navicon"></span></label>
+
         <div className="nav-title">
         The Old Town Bar, since 1892
         </div>
-        <div className="link-container">
+        <div className="link-container menu">
           <a className="nav-link" href='#about-us' >About us</a>
           <a className="nav-link" href="#hstry" >History</a>
           <a className="nav-link" href="menu" >Menu</a>
-          {/* <a className="nav-link" href="#reviews-scroll" >Reviews</a> */}
           <a className="nav-link" href="#contact-us"  >Contact Us</a>
-        </div>
+        </div> */}
+        <header class="header">
+          <a href="" className="logo">The Old Town Bar, since 1892</a>
+          <input className="menu-btn" type="checkbox" id="menu-btn" />
+          <label className="menu-icon" for="menu-btn">
+            <span class="navicon"></span>
+          </label>
+          <ul className="menu">
+            <li><a href='#about-us' >About us</a></li>
+            <li><a href="#hstry" >History</a></li>
+            <li><a href="menu" >Menu</a></li>
+            <li><a href="#contact-us"  >Contact Us</a></li>
+          </ul>
+        </header>
       </div>
     )
   }
